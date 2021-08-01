c12648fc5ec4452687ea6fc4ece12068cc142714
tharunya <tharunyareddy666@gmail.com>
2021-08-01 14:42:26 +0530

    Assignment-1

---
+++ com.training.assignment1/src/com/training/assignment1/EvenNumbers.java
@@ -0,0 +1,25 @@
+package com.training.assignment1;
+import java.util.Scanner;
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+		
+      sc.close();
+
+	}
+
+}
